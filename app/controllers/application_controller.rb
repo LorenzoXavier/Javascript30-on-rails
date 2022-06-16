c1b514a284477df083aa234d37fe5a27cb7ee107
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-06-17 00:26:18 +0100

    devise installed and navbar and footer added with social icon links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def authenticate_admin!
+
+    redirect_to root_path, status: :forbidden unless current_user.email == Rails.application.credentials.admin_user_email
+  end
 end
