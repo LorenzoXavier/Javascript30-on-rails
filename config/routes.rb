4896ad2445522d3110bac661693b73c9173f5baf
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-06-17 16:51:34 +0100

    CSS varaibles changed by JS complete

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   get '/drumkit', to: "pages#drumkit"
   get '/clock', to: "pages#clock"
-
+  get '/css-varaibles', to: "pages#css_variables"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
