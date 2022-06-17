319c830e7959cc7c4368ba6302f3d4a662c0b2b7
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-06-17 14:37:01 +0100

    changes to admin settings in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate_admin!
 
-    redirect_to root_path, status: :forbidden unless current_user.email == Rails.application.credentials.admin_user_email
+    redirect_to root_path, status: :forbidden unless current_user.email == "drumminloz@hotmail.com"
+     # current_user.email == Rails.application.credentials.admin_user_email
   end
 end
