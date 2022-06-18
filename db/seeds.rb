e344c606413b1283e82e33272c310afeb6f90f7c
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-06-18 15:46:11 +0100

    added a seed Item

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+item = Item.new(
+  title: "JS flex panel image gallery",
+  image: "https://res.cloudinary.com/du089vmk8/image/upload/v1655562850/JS_flexbox.png",
+  lesson: "5",
+  description: "Image gallery using JS to adjust flex box and text sizing when clicked",
+  link: "image-gallery",
+  code: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/views/pages/image_gallery.html.erb",
+  css: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/assets/stylesheets/pages/_image_gallery.scss"
+)
+puts "item created"
+item.save!
