e62306d81f39313057927849067114aefd657f4d
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-06-22 20:39:03 +0100

    seed file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 item = Item.new(
-  title: "JS flex panel image gallery",
-  image: "https://res.cloudinary.com/du089vmk8/image/upload/v1655562850/JS_flexbox.png",
-  lesson: "5",
-  description: "Image gallery using JS to adjust flex box and text sizing when clicked",
-  link: "image-gallery",
-  code: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/views/pages/image_gallery.html.erb",
-  css: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/assets/stylesheets/pages/_image_gallery.scss"
+  title: "Type ahead search function",
+  image: "https://res.cloudinary.com/du089vmk8/image/upload/v1655926454/search.png",
+  lesson: "6",
+  description: "JS search function that returns results by either state or city and shows the population.",
+  link: "type-ahead",
+  code: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/views/pages/type_ahead.html.erb",
+  css: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/assets/stylesheets/pages/_type_ahead.scss"
 )
 puts "item created"
 item.save!
