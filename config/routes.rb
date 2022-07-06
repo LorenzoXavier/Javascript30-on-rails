9f0bf69238947db78a2d357b635baeaa0bc75284
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-07-06 23:05:23 +0100

    functionality of brush strokes works with mouse up and down and adds paint in correct place

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/image-gallery', to: "pages#image_gallery"
   get '/type-ahead', to: "pages#type_ahead"
   get '/array-two', to: "pages#array_two"
+  get '/canvas', to: "pages#canvas"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
