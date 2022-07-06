ce59e934c5d0ad994bd8dfd98b7631b6261aefd3
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-07-06 20:46:41 +0100

    seeding file updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 item = Item.new(
-  title: "Type ahead search function",
-  image: "https://res.cloudinary.com/du089vmk8/image/upload/v1655926454/search.png",
-  lesson: "6",
-  description: "JS search function that returns results by either state or city and shows the population.",
-  link: "type-ahead",
-  code: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/views/pages/type_ahead.html.erb",
-  css: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/assets/stylesheets/pages/_type_ahead.scss"
+  title: "JS array task two",
+  image: "https://res.cloudinary.com/du089vmk8/image/upload/v1655557014/js_arrays.png",
+  lesson: "7",
+  description: "Second time working with arrays and manipulation of data - view in console on URL to see output",
+  link: "array-two",
+  code: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/views/pages/array_two.html.erb",
+  css: "https://github.com/LorenzoXavier/Javascript30-on-rails/blob/master/app/assets/stylesheets/pages/_array_one.scss"
 )
 puts "item created"
 item.save!
